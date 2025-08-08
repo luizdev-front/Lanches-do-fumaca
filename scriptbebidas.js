040dbf3174bc191e05f0e77f2febb796318b7149
Luiz Claudio- Dev <luizclaudio5423@gmail.com>
2025-08-08 13:47:37 -0300

    Atualizar o scriptbebidas.js
    sim

--- scriptbebidas.js
+++ scriptbebidas.js
@@ -38,3 +38,10 @@ function atualizarCarrinho() {
 
   carrinhoContainer.appendChild(totalDiv);
 }
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
