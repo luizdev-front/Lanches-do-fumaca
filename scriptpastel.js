112ccee0c92baa1a101dcbfb576ce4a2a705cf49
luiz Claudio <luizclaudio5423@gmail.com>
2025-08-08 21:01:38 -0300

    sim

--- scriptpastel.js
+++ scriptpastel.js
@@ -1,14 +1,22 @@
- const menuToggle = document.getElementById("menu-toggle");
- const navMenu = document.getElementById("nav-menu");
-
- menuToggle.addEventListener("click", (e) => {
-  e.stopPropagation();
-  navMenu.classList.toggle("open");
-});
-
-// Fecha se clicar fora
-document.addEventListener("click", (e) => {
-  if (!navMenu.contains(e.target) && e.target !== menuToggle) {
-    navMenu.classList.remove("open");
+function adicionarAoCarrinho(item, preco) {
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    carrinho.push({ item, preco });
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+    alert(`${item} foi adicionado ao carrinho!`);
   }
-});
+
+  // Controle do menu
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.getElementById("nav-menu");
+
+  menuToggle.addEventListener("click", (e) => {
+    e.stopPropagation();
+    navMenu.classList.toggle("open");
+  });
+
+  // Fecha se clicar fora
+  document.addEventListener("click", (e) => {
+    if (!navMenu.contains(e.target) && e.target !== menuToggle) {
+      navMenu.classList.remove("open");
+    }
+  });
